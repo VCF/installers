3d396c2f5df50b23f64e0da60c805b02e6b093e2
Charles Tilford <gitcommit@biocode.fastmail.fm>
2017-12-15 21:33:40 -0500

    Installation, warning of segfault

--- games/launchers/airships_launcher.sh
+++ games/launchers/airships_launcher.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-PROGDIR="airships-ea64-linux"
+## Location of the executable:
+PROGDIR="Airships"
 LAUNCH="Airships";
+## Location of the installer:
 INSTDIR="Desura"
 INSTNAME='airships-ea64-linux.zip'
-
+## Installation post-processing:
+INSTRENAME='airships-ea64-linux/Airships'
+## Run comment:
 PRERUN="Segmentation fault on Mint 18.3 Sylvia"
 
 my_dir="$(dirname "$0")"

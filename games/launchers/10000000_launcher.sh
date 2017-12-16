43a243ceccf859b3587e9b149de902de205323d6
Charles Tilford <gitcommit@biocode.fastmail.fm>
2017-12-15 20:45:34 -0500

    Added installer instructions

--- games/launchers/10000000_launcher.sh
+++ games/launchers/10000000_launcher.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+## Location of the executable:
 PROGDIR="10000000"
 LAUNCH="10000000.x86_64"
+## Location of the installer:
+INSTDIR="Humble Indie Bundle/10000000"
+INSTNAME='10000000_Linux.tar.gz'
+## Installation post-processing:
+INSTRENAME='Linux/10000000'
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"

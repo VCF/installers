745f8d9b0c9f8cf15c7210eab9dbfe43342f7c8b
Charles Tilford <gitcommit@biocode.fastmail.fm>
2017-12-16 11:09:10 -0500

    installer details

--- games/launchers/cosmochoria_launcher.sh
+++ games/launchers/cosmochoria_launcher.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-PROGDIR="Cosmochoria"
+## Location of the executable:
+PROGDIR="Cosmochoria/linux64"
 LAUNCH="cosmochoria"
-
-# Need to do:
-# sudo ln -sf /lib/x86_64-linux-gnu/libudev.so.1 /lib/x86_64-linux-gnu/libudev.so.0
+## Location of the installer:
+INSTDIR="Humble Indie Bundle/Cosmochoria"
+INSTNAME='Cosmochoria_*_Linux.zip'
+## Installation post-processing:
+INSTRENAME='Cosmochoria*/Cosmochoria'
+INSTSAVEDIR="$HOME/.config/Cosmochoria"
+PRERUN="If run fails, check LogFile.txt for 'libudev.so.0' error. Solve with:
+  sudo ln -s /lib/x86_64-linux-gnu/libudev.so.1 /lib/x86_64-linux-gnu/libudev.so.0
+"
 
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.steam/ubuntu12_32/steam-runtime/amd64/lib/x86_64-linux-gnu:$HOME/.steam/ubuntu12_32/steam-runtime/i386/lib/i386-linux-gnu
 

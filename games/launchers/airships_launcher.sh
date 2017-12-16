ab648efc321107073d512bccba508124cd4ec227
Charles Tilford <gitcommit@biocode.fastmail.fm>
2017-12-15 21:19:06 -0500

    segfault on Sylvia

--- games/launchers/airships_launcher.sh
+++ games/launchers/airships_launcher.sh
@@ -2,6 +2,10 @@
 
 PROGDIR="airships-ea64-linux"
 LAUNCH="Airships";
+INSTDIR="Desura"
+INSTNAME='airships-ea64-linux.zip'
+
+PRERUN="Segmentation fault on Mint 18.3 Sylvia"
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"

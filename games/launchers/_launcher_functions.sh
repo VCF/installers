68d83dd7c8c11ee96864d41935bbf0444b32675f
Charles Tilford <gitcommit@biocode.fastmail.fm>
2017-12-15 20:45:16 -0500

    Archive files, breaking out into functions

--- games/launchers/_launcher_functions.sh
+++ games/launchers/_launcher_functions.sh
@@ -78,6 +78,8 @@ Game directory not found, expected at:
         sleep 15
         return
     elif [ -s "$EXECUTABLE" ]; then
+        ## Not an executable. What is the extension?
+        ##     https://stackoverflow.com/a/965069
         ext="${LAUNCH##*.}"
         if [ "$ext" = 'jar' ]; then
             msg 32 "  Running Java file \"$EXECUTABLE\""
@@ -94,7 +96,7 @@ Game directory not found, expected at:
     fi
 
     ## Nothing found. Can we install?
-    if [[ ! -z "$TRIEDINSTALL" ]]
+    if [[ -z "$TRIEDINSTALL" ]]; then
        installGame
        return
     fi
@@ -119,13 +121,58 @@ No guidance given on where to find installer
         echo ""
         return
     fi
+    findInstaller
+        
+    if [[ -z "$installer" ]]; then
+        ## Installer not found
+        msg 31 "  Could not find installer '$INSTNAME' in:
+${TryDir[@]}"
+        echo ""
+        return
+    fi
+
+    cd "$GAMEDIR"
+    determineSuffix
+
+    if [[ $sfx == "sh" ]]; then
+        TRIEDINSTALL="SHELL: $installer"
+        if [[ -x "$installer" ]]; then
+            installShell
+        else
+            failedNotExecutable
+            return
+        fi
+    elif [[ $sfx == "bz2" || $sfx == "bzip2" ]]; then
+        ## bzip archive
+        installBzip
+    elif [[ $sfx == "gz" ]]; then
+        ## gzip archive
+        installGzip
+    else
+        ## No idea!
+        failedUnknown
+        return
+    fi
+
+    autoRename
     
-    ## Check first to see if the installer is in Downloads or ToFile;
-    ## If so, use one of those. 
+    msg "30;102" "
+Installer finished, attempting launch
+  If successful, be sure to also link save files to consistent location:
+  ~/confFiles/games/makeGameLinks.sh
+"
+    showComments
+    find_and_run_executable
+}
+
+function findInstaller {
+    ## Check first to see if the installer is in ~/Downloads or
+    ## ~/ToFile; If so, use one of those.
     TryDir[0]="$HOME/Downloads"
     TryDir[1]="$HOME/ToFile"
     if [[ ! -z "$INSTDIR" ]]; then
-        ## Also use INSTDIR as default location, if set
+        ## Also use INSTDIR as default location, if it has been set by
+        ## the launcher
         isAbsPath=`echo "$INSTDIR" | grep '^/'`
         if [[ -z "$isAbsPath" ]]; then
             ## Appears to be a relative path
@@ -137,80 +184,139 @@ No guidance given on where to find installer
     fi
 
     for dir in "${TryDir[@]}"; do
+        ## List by modified date, if there are more than one match
+        ## take the most recent one. Allows use of pattern eg:
+        ##    myGame_*.sh -> myGame_1.3.sh, myGame_1.4.2.sh etc
         installer=`ls -1t "$dir"/$INSTNAME 2>/dev/null | head -n1 `
-        [[ -z "$installer" ]] || break
+        [[ -z "$installer" ]] || break # Take first example we find
     done
-        
-    if [[ -z "$installer" ]]; then
-        ## Installer not found
-        msg 31 "  Could not find installer '$INSTNAME' in:
-${TryDir[@]}"
-        echo ""
-        return
-    fi
+}
 
-    cd "$GAMEDIR"
+function determineSuffix {
+    ##  Lower case in bash: https://stackoverflow.com/a/2264537
+    ## Parameter Expansion: https://stackoverflow.com/a/965069
+    sfx=`echo "${installer##*.}" | tr '[:upper:]' '[:lower:]'`
+    ## Also see if this looks like a TAR archive
+    unTar=`isTarArchive "$installer"`
+}
 
-    ## Lower case in bash: https://stackoverflow.com/a/2264537
-    sfx=`echo "$installer" | egrep -o '\.(bzip2|bz2|sh)$' | tr '[:upper:]' '[:lower:]'`
-    unTar=`doUnTar "$installer"`
-    if [[ $sfx == ".sh" ]]; then
-        TRIEDINSTALL="SHELL: $installer"
-        if [[ -x "$installer" ]]; then
-            msg "35" "
+function isTarArchive {
+    isTar=`echo "$1" | grep -i '\.tar\.'`
+    unTarLine=""
+    [[ -z "$isTar" ]] || unTarLine=" | tar -xvf -"
+    echo "$unTarLine"
+}
+
+function installShell {
+    msg "35" "
 Preparing to install:
   $installer
   If asked for a location, use: $GAMEDIR
            Subfolder should be: $PROGDIR
 "
-            $installer
-        else
-            msg "31" "
+    $installer
+}
+
+function installBzip {
+    TRIEDINSTALL="BZIP2 Archive: $installer"
+    msg "35" "
+Preparing to extract:
+  $installer
+"
+    ## Extracting Bzip: https://superuser.com/a/480951
+    cmd="bzip2 --stdout --verbose --decompress --keep \"$installer\" $unTar"
+    ## Command literal with eval: https://stackoverflow.com/a/2355242
+    eval "$cmd"
+}
+
+function installGzip {
+    TRIEDINSTALL="GZIP Archive: $installer"
+    msg "35" "
+Preparing to extract:
+  $installer
+"
+    ## Extracting Bzip: https://superuser.com/a/480951
+    cmd="gzip --stdout --verbose --decompress --keep \"$installer\" $unTar"
+    ## Command literal with eval: https://stackoverflow.com/a/2355242
+    eval "$cmd"
+}
+
+function failedNotExecutable {
+    msg "31" "
 Found installer:
   $installer
   ... but it is not executable. Run:
     chmod u+x "$installer"
 "
-            return
-        fi
-    elif [[ $sfx == ".bz2" || $sfx == ".bzip2" ]]; then
-        TRIEDINSTALL="BZIP2 Archive: $installer"
-        msg "35" "
-Preparing to extract:
-  $installer
-"
-        ## Extracting Bzip: https://superuser.com/a/480951
-        cmd="bzip2 --stdout --verbose --decompress --keep \"$installer\" $unTar"
-        ## Command literal with eval: https://stackoverflow.com/a/2355242
-        eval "$cmd"
-    else
-        msg "31" "
+}
+
+function failedUnknown {
+    msg "31" "
 Found installer:
   $installer
   ... but did not know what to do with it.
 "
-        TRIEDINSTALL="UNKNOWN"
-        return
-    fi
-    msg "30;102" "
-Installer finished, attempting launch
-  If successful, be sure to also link save files to consistent location:
-  ~/confFiles/games/makeGameLinks.sh
-"
-    ## Indicate if additional packages are needed:
+    TRIEDINSTALL="UNKNOWN"
+}
+
+function showComments {
+    ## Indicate if additional packages are indicated as needed:
     [[ -z "$INSTAPT" ]] || msg "43;34" "
-You may need to install additional libraries / software:
+You may need to install additional libraries / software using:
     sudo apt install $INSTAPT"
 
     ## Indicate if help (URL, man page, etc) is provided:
     [[ -z "$INSTHELP" ]] || msg "36" "Installation Help: $INSTHELP"
-    find_and_run_executable
+    
 }
 
+function autoRename {
+    [[ -z "INSTRENAME" ]] && return
+    ## A variable has been set to rename something. This can be useful
+    ## when an archive has a generic name like 'linux' as the
+    ## top-level folder
+    
+    ## String splitting: https://stackoverflow.com/a/918931
+    IFS='/' read -ra RenameBits <<< "$INSTRENAME"
+    reFrom="${RenameBits[0]}"
+    reTo="${RenameBits[1]}"
+    
+    if [[ -z "$reFrom" || -z "$reTo" ]]; then
+        msg "31" "
+The variable INSTRENAME was set, indicating that something should be renamed
+  But I only found one part (there should be two separated by '/'):
+    $INSTRENAME
+  This may cause problems ...
+"
+        return
+    fi
 
-function doUnTar {
-    isTar=`echo "$1" | grep -i '\.tar\.'`
-    unTarLine=""
-    [[ -z "$isTar" ]] || unTarLine=" | tar -xvf -"
-    echo "$unTarLine"
+    if [[ ! -e "$reFrom" ]]; then
+        msg "31" "
+Rename request
+  From: \"$reFrom\"
+    To: \"$reTo\"
+  But I could not find \"$reFrom\" ...
+  This may cause problems ...
+"
+        return
+    fi
+
+    if [[ -e "$reTo" ]]; then
+        msg "31" "
+Rename request
+  From: \"$reFrom\"
+    To: \"$reTo\"
+
+  But \"$reTo\" already exists! Please resolve (renaming, moving
+  or deleting the 'To' file), this will almost certainly cause
+  problems!
+"
+        return
+    fi
+
+    ## We have identified the "from" file, and the "to" file does not
+    ## exist. Rename it:
+    mv "$reFrom" "$reTo"
+    msg "32" "Renamed '$reFrom' to '$reTo'"
 }

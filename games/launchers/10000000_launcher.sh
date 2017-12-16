0c112b42b102e9087a9bf3ccd938878858d73229
Charles Tilford <gitcommit@biocode.fastmail.fm>
2017-12-16 11:05:58 -0500

    save location

--- games/launchers/10000000_launcher.sh
+++ games/launchers/10000000_launcher.sh
@@ -8,6 +8,7 @@ INSTDIR="Humble Indie Bundle/10000000"
 INSTNAME='10000000_Linux.tar.gz'
 ## Installation post-processing:
 INSTRENAME='Linux/10000000'
+INSTSAVEDIR="$HOME/.config/unity3d/EightyEightGames/10000000"
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"
--- games/launchers/factorio_launcher.sh
+++ games/launchers/factorio_launcher.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+## Location of the executable:
 PROGDIR="Factorio"
 LAUNCH="start.sh"
+## Location of the installer:
 INSTDIR="GOG/Factorio"
 INSTNAME='factorio_en_*.sh'
+## Installation post-processing:
+INSTSAVEDIR="$HOME/Programs/Factorio/game/saves"
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"

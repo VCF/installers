6f8170abbf73d86ac8def5fc6fba13f30de69c8b
Charles Tilford <github@biocode.fastmail.fm>
2017-12-19 21:31:43 -0500

    terraria

---
+++ games/launchers/terraria_launcher.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Location of the executable:
+PROGDIR="Terraria"
+LAUNCH="start.sh"
+## Location of the installer:
+INSTDIR="GOG/Terraria"
+INSTNAME='terraria_en_*.sh'
+## Installation post-processing:
+INSTSAVEDIR="$HOME/.local/share/Terraria"
+INSTICON="Terraria.png"
+
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+find_and_run_executable

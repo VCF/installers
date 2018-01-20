084c41ae2057da29eed30c956245b06cb99b9519
Charles Tilford <github@biocode.fastmail.fm>
2018-01-19 21:25:08 -0500

    icon and URL commentary

--- games/launchers/arduino_launcher.sh
+++ games/launchers/arduino_launcher.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+## IDE for Arduino
+##   Even on Mint 18.3 in 2018 the Mint repo was pretty far behind:
+##   https://www.arduino.cc/en/main/software
+
 ## Location of the executable:
 PROGDIR="Arduino"
 LAUNCH="arduino"
@@ -8,6 +12,7 @@ INSTDIR="OpenSource/Arduino"
 INSTNAME='arduino-*-linux64.tar.xz'
 ## Installation post-processing:
 INSTRENAME='arduino-*/'$PROGDIR
+INSTICON="Arduino.png"
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"

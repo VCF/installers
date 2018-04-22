82444721e81120bf91a834c4c6d24e4ac09a1a04
Charles Tilford <github@biocode.fastmail.fm>
2018-04-22 11:17:49 -0400

    Force 32 bit prefix, auto-alter configuration file

--- games/launchers/legoharrypotter1-4_launcher.sh
+++ games/launchers/legoharrypotter1-4_launcher.sh
@@ -6,24 +6,38 @@
 PROGDIR="LegoHarryPotter1-4"
 #PROGSUBDIR="Silent Storm"
 LAUNCH="LEGOHarryPotter.exe"
+
 ## Location of the installer:
 INSTDIR="GOG/Lego Harry Potter"
 INSTNAME='setup_lego_harry_potter_1-4_*.exe'
+
+## Wine configuration:
+wineBits="32"
+WINEPREFIX="$HOME/Wine$wineBits"
+WINEARCH="win$wineBits"
+
 ## Installation post-processing:
 ## Capitalization may vary:
 WINETARGET="GOG Games/LEGO Harry Potter 1-4"
-# INSTSAVEDIR="$PROGSUBDIR/save"
+INSTSAVEDIR="drive_c/users/$USER/Application Data/WB Games/LEGO® Harry Potter™/SavedGames"
+
 INSTICON="$PROGDIR.png"
 ## Notes:
 PRERUN="
 WineHQ Platinum
   https://appdb.winehq.org/objectManager.php?sClass=application&iId=11718
 
-  Supposedly. In my hands, it's Garbage - graphics are massively messed up,
-  psychadelic blocks of color.
-
-  Failed in both Wine1.6 and Wine3.0
+  Requires a 32 bit Wine prefix. Works in wine-3.0
 "
+
+function INSTFUNCTION {
+    ## https://appdb.winehq.org/objectManager.php?sClass=version&iId=20680
+    wineDriveC
+    confFile="$cDrive/users/$USER/Application Data/WB Games/LEGO® Harry Potter™/pcconfig.txt"
+    ## https://stackoverflow.com/a/11245501 # Tell sed to change the line
+    sed -i '/DesiredDynamicLightQuality  2/c\DesiredDynamicLightQuality  0' "$confFile"
+
+}
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"

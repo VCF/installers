e6031563bd2e52ce53e3705c80fbb72e177b31b8
Charles Tilford <github@biocode.fastmail.fm>
2018-12-21 23:36:13 -0500

    Deep Sky Derelicts

---
+++ games/launchers/deepskyderelicts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Location of the executable:
+PROGDIR="Deep Sky Derelicts"
+LAUNCH="start.sh"
+## Location of the installer:
+INSTDIR="GOG/$PROGDIR"
+INSTNAME='deep_sky_derelicts_*.sh'
+## Installation post-processing:
+INSTSAVEDIR="$HOME/.config/unity3d/Snowhound/Deep\ Sky\ Derelicts"
+INSTICON="$PROGDIR.jpg"
+## Notes:
+
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+find_and_run_executable "$@"

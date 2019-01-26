a9f737ac11a6c98b38e75cbb76c154ff9ca20c4b
Charles Tilford <github@biocode.fastmail.fm>
2019-01-26 15:15:23 -0500

    Craft the World wine wrapper

---
+++ games/launchers/crafttheworld_launcher.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+## Location of the executable:
+PROGDIR="Craft the World"
+LAUNCH="CraftWorld.exe"
+
+## Location of the installer:
+INSTDIR="GOG/$PROGDIR"
+INSTNAME='setup_craft_the_world_*.exe'
+
+## Wine configuration:
+wineBits="32"
+WINEPREFIX="$HOME/Wine$wineBits"
+WINEARCH="win$wineBits"
+WINETARGET="GOG Games/Craft The World"
+
+## Installation post-processing:
+INSTSAVEDIR="drive_c/users/$USER/Application Data/dekovir/crafttheworld/saves"
+
+INSTICON="$PROGDIR.png"
+## Notes:
+PRERUN="
+WineHQ Platinum
+  https://appdb.winehq.org/objectManager.php?sClass=application&iId=16032
+"
+
+find_and_run_executable "$@"

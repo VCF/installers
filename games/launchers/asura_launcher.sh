ebeb802007ab74f5295bf689c601edf6a79896b1
Charles Tilford <github@biocode.fastmail.fm>
2019-02-23 23:40:07 -0500

    Asura

---
+++ games/launchers/asura_launcher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Location of the executable:
+PROGDIR="Asura"
+LAUNCH="start.sh"
+## Location of the installer:
+INSTDIR="GOG/$PROGDIR"
+INSTNAME='asura_en_*.sh'
+## Installation post-processing:
+# INSTSAVEDIR="$HOME/.config/unity3d/Convoy Games/Convoy"
+INSTICON="$PROGDIR.png"
+
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+find_and_run_executable "$@"
+

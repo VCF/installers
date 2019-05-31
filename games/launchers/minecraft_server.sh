61f357f9a18d085424aecc723c956c4bb6c2488a
Charles Tilford <gitcommit@biocode.fastmail.fm>
2019-05-30 21:42:59 -0400

    New bedrock server for minecraft

---
+++ games/launchers/minecraft_server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+## Location of the executable:
+PROGDIR="Minecraft Server"
+LAUNCH="bedrock_server"
+## Location of the installer:
+INSTDIR="/abyss/Common/$PROGDIR/Executable"
+INSTNAME='bedrock-server-*.zip'
+INSTHELP="To update, visit:
+  https://www.minecraft.net/en-us/download/server/bedrock/
+"
+## Installation post-processing:
+# INSTSAVEDIR="$HOME/.minecraft/saves"
+INSTICON="$PROGDIR.png"
+UNPACKDIR="$PROGDIR"
+NOREDIRECT="TRUE"
+
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+msg "$FgGreen" "
+
+Settings for your server are in:
+  $GAMEDIR/$PROGDIR/server.properties
+
+"
+
+find_and_run_executable "$@"
+

217b97175225f27f4a2722a2a6d724fc381a9bfe
Charles Tilford <gitcommit@biocode.fastmail.fm>
2019-05-30 21:42:33 -0400

    Huh, didn't work with OpenJDK 11

--- games/launchers/minecraft_client.sh
+++ games/launchers/minecraft_client.sh
@@ -3,7 +3,9 @@
 ## Location of the executable:
 PROGDIR="Minecraft"
 LAUNCH="minecraft.jar"
+JAVACMD="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
 ## Location of the installer:
+APTPACKAGES="openjdk-8-jre"
 INSTDIR="/abyss/Common/$PROGDIR/Executable"
 INSTNAME="$LAUNCH"
 INSTCOPY=true # Installation is just a copy
@@ -18,3 +20,7 @@ my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"
 
 find_and_run_executable "$@"
+
+## Minecraft apparently needs JRE 8
+##   https://askubuntu.com/a/1030255
+##   I can confirm it failed with 11, but worked for 8 (Ubuntu 18.04.2 LTS)

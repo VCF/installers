ddf6d2d5a9f0b94dec08253c0a5e798b521bad09
Charles Tilford <gitcommit@biocode.fastmail.fm>
2019-12-06 23:30:51 -0500

    comment on why Steam's libcrypto is being used

--- games/launchers/dungeonsouls_launcher.sh
+++ games/launchers/dungeonsouls_launcher.sh
@@ -13,7 +13,9 @@ INSTNAME='dungeon_souls_en_*.sh'
 INSTSAVEDIR="$HOME/.config/DungeonSouls"
 INSTICON="$PROGDIR.png"
 
-
+## Uses a deprecated (insecure) version of libcrypto that wasn't
+## available from the Ubuntu repositories, but I found a copy in
+## Steam's library folder:
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.steam/ubuntu12_32/steam-runtime/i386/lib/i386-linux-gnu/"
 
 find_and_run_executable "$@"

17e1a106056ebee694d30e28e0285d41c83c6695
Charles Tilford <gitcommit@biocode.fastmail.fm>
2019-12-07 13:44:34 -0500

    simple util to find recently modified files in HOME

---
+++ games/launchers/findRecentlyModified.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Find recently (24hr) modified files in home directory, filtering
+## out irrelevant matches
+
+find "$HOME" -mtime -1 |\
+    egrep -v '(Wine|waterfox|gimp|cache|Browser|confFiles|emacs)' |\
+    egrep -v '(Desktop|Programs|icons|GameFiles|zeitgeist|generalUtilities)' |\
+    egrep -v '(installers|pulse|Downloads)' |\
+    less -S

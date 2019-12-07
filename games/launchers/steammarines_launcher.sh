26bc7b328b1715f3cb41783811a39a8aa8145868
Charles Tilford <gitcommit@biocode.fastmail.fm>
2019-12-06 22:55:39 -0500

    Steam Marines

---
+++ games/launchers/steammarines_launcher.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+## Location of the executable:
+PROGDIR="Steam Marines"
+LAUNCH="run.sh"
+## Location of the installer:
+INSTDIR="Humble Indie Bundle/$PROGDIR"
+INSTNAME='Steam_Marines_*.zip'
+## Installation post-processing:
+INSTRENAME='Steam_Marines_*/'"$PROGDIR"
+## Save directory is under a literal '~' - probably a shell error on their part
+## THis causes problems making the symlink
+# INSTSAVEDIR="$GAMEDIR/$PROGDIR/"'~/Worthless Bums/Steam Marines/'
+INSTICON="$PROGDIR.png"
+## A couple 32-bit packages needed on amd64 systems:
+APTPACKAGES="
+libxft2:i386
+libsdl1.2debian:i386
+"
+
+function INSTFUNCTION {
+    ## Executable needs to be set on main program
+    chmod u+x "$GAMEDIR/$PROGDIR/Steam Marines"
+}
+
+find_and_run_executable "$@"
+

286a996ff20d2c40b7ef970947be303270429c27
Charles Tilford <gitcommit@biocode.fastmail.fm>
2019-12-08 16:18:29 -0500

    Cryptark

--- games/launchers/cryptark_launcher.sh
+++ games/launchers/cryptark_launcher.sh
@@ -2,15 +2,22 @@
 
 ## Location of the executable:
 PROGDIR="Cryptark"
-LAUNCH="Crawl.x86_64"
+LAUNCH="Cryptark.bin.x86_64"
 ## Location of the installer:
 INSTDIR="Humble Indie Bundle/$PROGDIR"
 INSTNAME='cryptark-*-bin-linux'
+UNPACKDIR="$PROGDIR"
 ## Installation post-processing:
 INSTICON="$PROGDIR.png"
-## INSTSAVEDIR="$HOME/.config/unity3d/Powerhoof/Crawl"
+INSTSAVEDIR="$HOME/.local/share/Cryptark"
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"
+
+PREINST="
+
+  When asked, specify install location as:  
+    $GAMEDIR/$PROGDIR                       
+"
 
 find_and_run_executable "$@"

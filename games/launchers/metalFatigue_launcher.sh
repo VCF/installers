73a6b6a792de0be6896d11c2645e57130bb1428f
Charles Tilford <gitcommit@biocode.fastmail.fm>
2019-12-15 19:57:45 -0500

    Metal Fatigue

---
+++ games/launchers/metalFatigue_launcher.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+## Location of the executable:
+PROGDIR="Metal Fatigue"
+LAUNCH="MFatigue.exe"
+
+## Location of the installer:
+INSTDIR="GOG/$PROGDIR"
+INSTNAME='setup_metal_fatigue_*.exe'
+
+## Wine configuration:
+wineBits="32"
+WINEPREFIX="$HOME/Wine$wineBits"
+WINEARCH="win$wineBits"
+WINETARGET="GOG Games/Metal Fatigue"
+
+## Installation post-processing:
+INSTSAVEDIR="drive_c/GOG Games/Metal Fatigue/Save"
+
+INSTICON="$PROGDIR.jpg"
+## Notes:
+PRERUN="
+WineHQ Platinum
+  https://appdb.winehq.org/objectManager.php?sClass=version&iId=37034
+"
+
+find_and_run_executable "$@"

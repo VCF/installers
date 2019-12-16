8f27f960c76cdf6f88a3d25dfa49423c9a81a029
Charles Tilford <gitcommit@biocode.fastmail.fm>
2019-12-15 20:31:33 -0500

    mask out some background

--- games/launchers/findRecentlyModified.sh
+++ games/launchers/findRecentlyModified.sh
@@ -6,5 +6,6 @@
 find "$HOME" -mtime -1 |\
     egrep -v '(Wine|waterfox|gimp|cache|Browser|confFiles|emacs)' |\
     egrep -v '(Desktop|Programs|icons|GameFiles|zeitgeist|generalUtilities)' |\
-    egrep -v '(installers|pulse|Downloads)' |\
+    egrep -v '(installers|pulse|Downloads|Trash|QMapShack|dolphin)' |\
+    egrep -v '(RecentDocuments|kactivitymanagerd)' |\
     less -S

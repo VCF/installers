98a24372e8d3eeeb1392f7e4054144aba14ebcae
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-01-20 16:53:12 -0500

    Backup profile dir. launch directly rather than from desktop link

--- applications/tor_launcher.sh
+++ applications/tor_launcher.sh
@@ -4,7 +4,8 @@
 
 ## Location of the executable:
 PROGDIR="Tor"
-LAUNCH="start-tor-browser.desktop"
+LAUNCH="Browser/start-tor-browser"
+
 ## Location of the installer:
 if [[ -z "$(uname -m | grep x86_64)" ]]; then
     ## 32-bit
@@ -15,8 +16,7 @@ else
     INSTNAME='tor-browser-linux64-*_en-US.tar.xz'
     INSTDIR="Linux/Tor"
 fi
-## No save files involved:
-INSTSAVEDIR='NONE'
+
 ## Installation post-processing:
 INSTRENAME='tor-browser_en-US/'$PROGDIR
 INSTICON="tor.png"
@@ -24,10 +24,14 @@ NOTINTERM="Don't run in a terminal"
 
 PRERUN="
 https://www.torproject.org/projects/torbrowser.html.en
-
 "
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"
+
+## Tor does NOT like having the profile directory relocated to another
+## spot and then symlinked. So instead we will backup the profile
+## folder "in place" using rsync:
+RSYNCDIR="$GAMEDIR/$PROGDIR/Browser/TorBrowser/Data/Browser/profile.default"
 
 find_and_run_executable "$@"

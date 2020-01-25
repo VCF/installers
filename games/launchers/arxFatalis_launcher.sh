f261304c89d61b3d961840b845a62cb32476fb71
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-01-25 14:42:08 -0500

    Arx Fatalis

---
+++ games/launchers/arxFatalis_launcher.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+## Location of the executable:
+PROGDIR="Arx Fatalis"
+LAUNCH="ARX.exe"
+
+## Location of the installer on your file system:
+INSTDIR="GOG/$PROGDIR"
+INSTNAME='setup_arx_fatalis.exe'
+
+## Source Utility functions
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+###### configuration:
+## Should we use 32- or 64-bit environment?
+wineBits="32"
+## Define the prefix (folder) for the Wine installation:
+WINEPREFIX="$HOME/Wine$wineBits"
+## Set the architecture being used for Wine:
+WINEARCH="win$wineBits"
+## The location of the installed program in drive_c:
+WINETARGET="Program Files/GOG.com/Arx Fatalis"
+## Switches passed to Wine when running application
+## WINEARGS="-windowed"
+
+## Filename of launcher icon (looks in default folders):
+INSTICON="$PROGDIR.png"
+
+## Specify any required APT packages:
+## APTPACKAGES='libsdl2-mixer-2.0-0'
+
+## Location of the programs save files / config:
+INSTSAVEDIR="$GAMEDIR/$PROGDIR/Save"
+## Flag to use if backup should use rsync rather than tar/gzip:
+DORSYNC="yes"
+## Flag that prevents backups if set, and shows the provided message:
+## NOAUTOBACK="Backups take too long to run"
+
+## Custom function to run after installation:
+function INSTFUNCTION {
+    PDIR="$GAMEDIR/$PROGDIR"
+    echo "$PDIR/cfg_default.ini" |
+        sed 's/magic_mode_k0=---/magic_mode_k0=LControl/' >
+    "$PDIR/cfg.ini"
+}
+## Custom function to run each time just before running the program:
+#function RUNFUNCTION {
+#  echo "For example, configuring the system in some way"
+#}
+
+## Notes shown before program is executed
+PRERUN="
+Please configure winecfg to turn off window manager control:
+  WINEARCH=win32 WINEPREFIX=$HOME/Wine32 winecfg 
+"
+
+find_and_run_executable "$@"

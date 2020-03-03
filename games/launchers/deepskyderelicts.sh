2b29408363aaf731d0d5c1cf4685229ec7b2e5b6
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-03-02 21:06:57 -0500

    DLC

--- games/launchers/deepskyderelicts.sh
+++ games/launchers/deepskyderelicts.sh
@@ -5,7 +5,7 @@ PROGDIR="Deep Sky Derelicts"
 LAUNCH="start.sh"
 ## Location of the installer:
 INSTDIR="GOG/$PROGDIR"
-INSTNAME='deep_sky_derelicts_*.sh'
+INSTNAME='deep_sky_derelicts_1*.sh'
 ## Installation post-processing:
 INSTSAVEDIR="$HOME/.config/unity3d/Snowhound/Deep Sky Derelicts"
 INSTICON="$PROGDIR.jpg"
@@ -13,5 +13,23 @@ INSTICON="$PROGDIR.jpg"
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"
+
+## Custom function to run after installation:
+function INSTFUNCTION {
+    ## Also install DLC
+    OLDINST="$INSTNAME"
+    INSTNAME='deep_sky_derelicts_new_prospects_*.sh'
+    installer=""
+    findInstaller
+    if [[ -z "$installer" ]]; then
+        msg "$FgYellow" "  DLC absent: $INSTNAME"
+    else
+        msg "$FgBlue" "  Installing DLC:
+    $installer
+"
+        installShell
+    fi
+    INSTNAME="$OLDINST"
+}
 
 find_and_run_executable "$@"

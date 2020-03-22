bb513da2a854686701671a8e8000759d27dbcea8
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-03-22 16:03:52 -0400

    trying to resolve library issues

--- games/launchers/sublevelZero_launcher.sh
+++ games/launchers/sublevelZero_launcher.sh
@@ -8,6 +8,10 @@ LAUNCH="start.sh"
 INSTDIR="GOG/$PROGDIR"
 INSTNAME='gog_sublevel_zero_*.sh'
 
+## Specify any required APT packages:
+## Make sure a couple 32-bit packages are available:
+APTPACKAGES='libxcursor1:i386 libxrandr2:i386'
+
 ## Source Utility functions
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"
@@ -17,5 +21,12 @@ INSTICON="$PROGDIR.png"
 
 ## Location of the programs save files / config:
 INSTSAVEDIR="$HOME/.config/unity3d/SIGTRAP Games/SublevelZero"
+
+## Identical problem on a different game (core dump on Unity import of
+## game controller config) where the program wasn't finding local
+## 32-bit libraries
+
+## https://steamcommunity.com/app/416770/discussions/0/492379439687359165/?ctp=2#c492379439688639977
+export LD_LIBRARY_PATH=. %command%
 
 find_and_run_executable "$@"

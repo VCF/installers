e7e7b114352fb8f3a8eca19ccc5308918265c16b
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-04-26 09:54:13 -0400

    save dir

--- games/launchers/darkestDungeon_launcher.sh
+++ games/launchers/darkestDungeon_launcher.sh
@@ -3,10 +3,6 @@
 ## Location of the executable:
 PROGDIR="Darkest Dungeon"
 LAUNCH="start.sh"
-## Arguments to pass to the executable when running:
-## LAUNCHARGS="-nogui -res=640x320"
-## Subdirectory in the program folder to change to before running:
-## PROGSUBDIR="bin"
 
 ## Location of the installer on your file system:
 INSTDIR="GOG/$PROGDIR"
@@ -19,17 +15,8 @@ my_dir="$(dirname "$0")"
 ## Filename of launcher icon (looks in default folders):
 INSTICON="$PROGDIR.png"
 
-## Specify any required APT packages:
-## APTPACKAGES='libsdl2-mixer-2.0-0'
-## Normally a new terminal is opened to launch. Flag to prevent this:
-## NOTINTERM="No launching terminal"
-
 ## Location of the programs save files / config:
-## INSTSAVEDIR="$HOME/.config/foobar"
-## Flag to use if backup should use rsync rather than tar/gzip:
-## DORSYNC="yes"
-## Flag that prevents backups if set, and shows the provided message:
-## NOAUTOBACK="Backups take too long to run"
+INSTSAVEDIR="$HOME/.local/share/Red Hook Studios/Darkest"
 
 function addDLC {
     DLCFILE="$1"
@@ -50,14 +37,9 @@ function addDLC {
 
 ## Custom function to run after installation:
 function INSTFUNCTION {
-    echo "Installing DLCs"
+    msg "$FgMagenta" "Installing DLCs"
     addDLC 'darkest_dungeon_musketeer_dlc_en_*.sh'
     addDLC 'darkest_dungeon_the_color_of_madness_*.sh'
 }
-
-## Notes shown before program is executed
-#PRERUN="
-#  Any message you want the installer to display before running
-#"
 
 find_and_run_executable "$@"

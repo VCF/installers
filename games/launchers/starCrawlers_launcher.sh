40ee1627ee02f21160f84a2a46243f80bffb7497
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-04-29 22:21:48 -0400

    save dir

--- games/launchers/starCrawlers_launcher.sh
+++ games/launchers/starCrawlers_launcher.sh
@@ -2,7 +2,7 @@
 
 ## Location of the executable:
 PROGDIR="StarCrawlers"
-LAUNCH="DID NOT RECORD"
+LAUNCH="start.sh"
 
 ## Location of the installer on your file system:
 INSTDIR="GOG/$PROGDIR"
@@ -10,5 +10,12 @@ INSTNAME='starcrawlers_*.sh'
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"
+
+## Filename of launcher icon (looks in default folders):
+INSTICON="$PROGDIR.png"
+
+## Location of the programs save files / config:
+INSTSAVEDIR="$HOME/.config/unity3d/Juggernaut Games/StarCrawlers"
+
 
 find_and_run_executable "$@"

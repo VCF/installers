fdd6fd0139bb0e65267a1486218704718c3ec5d8
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-05-21 18:26:46 -0400

    fix executable path, add icon and save location

--- games/launchers/torchlight2_launcher.sh
+++ games/launchers/torchlight2_launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROGDIR="Torchlight"
+PROGDIR="Torchlight2"
 LAUNCH="Torchlight2.bin.x86_64"
 
 ## Location of the installer:
@@ -9,5 +9,11 @@ INSTNAME='Torchlight2-linux-*.sh'
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"
+
+## Filename of launcher icon (looks in default folders):
+INSTICON="$PROGDIR.png"
+
+## Location of the programs save files / config:
+INSTSAVEDIR="$HOME/.local/share/Runic Games/Torchlight 2"
 
 find_and_run_executable "$@"

a9794121330c5b451d50931f20a287bdcec26355
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-06-06 12:33:34 -0400

    setup scripts for new hardware

---
+++ newSystem/88-office-apt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Packages used on 'secure' office computer
+
+sudo apt-get install -y \
+     tesseract-ocr tesseract-ocr-eng \
+     darktable   \
+     gnucash     \
+     handbrake   \
+     thunderbird
+

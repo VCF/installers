f2b7644c905452cd2fd357fcb1286f45dd70eedf
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-06-06 10:59:12 -0400

    Add option to lock system, triggered by presence of hidden file

--- scripts/UbuntuSleep/ubuntuSleep.sh
+++ scripts/UbuntuSleep/ubuntuSleep.sh
@@ -6,34 +6,64 @@
 ## suspsend seems to change every two years. This script is designed
 ## to codify the different mechanisms
 
-HNAME=`hostname`
+HNAME=$(hostname)
 
-# Thunderbird over NFS causes issues...
+## On past systems, active instances of Thunderbird (with mail files
+## on an NFS server) or VirtualBox caused problems when suspending, so
+## I killed them. That seems to be resolved now. Leaving for posterity:
+
 ## killall --quiet --signal INT thunderbird
-
-# Also shutdown any virtualbox instances:
 ## killall --quiet --signal INT VirtualBox
 
+echo "Suspend request at $(date)"
 ## Give the mouse time to settle down
 sleep 2
 
-HASGSC=`which gnome-screensaver-command`
-HASPMI=`which pmi`
-ISSYSTEMD=`which systemctl`
+HASGSC=$(which gnome-screensaver-command)
+HASCSC=$(which cinnamon-screensaver-command)
+HASMSC=$(which mate-screensaver-command)
+HASPMI=$(which pmi)
+ISSYSTEMD=$(which systemctl)
 
-if [[ $HNAME != 'shrew' && ! -z "$HASGSC" ]]; then
-    ## I forget why I wasn't doing this on shrew ...
-    ## How do I lock the screen from a terminal?
-    ##   https://askubuntu.com/a/184732
-    echo "Lock via screensaver"
-    gnome-screensaver-command -l &
-    
-elif [[ ! -z "$ISSYSTEMD" ]]; then
+## If you also wish the computer to lock, place a non-empty file (any
+## content) at this location:
+doLockFile="$HOME/.lockOnSuspend"
+
+if [[ -s "$doLockFile" ]]; then
+    ## Locking the screen ...
+    echo "Request to lock screen indicated by file: $doLockFile"
+    ## ... but this ALSO is polymorphic across Ubuntu variants. Sigh.
+    ##   https://forums.linuxmint.com/viewtopic.php?t=307131
+    LockCommand=""
+    if [[ -n "$HASGSC" ]]; then
+        ## Gnome-based screensaver
+        LockCommand="$HASGSC"
+    elif [[ -n "$HASCSC" ]]; then
+        ## Still Gnome, but Linux Mint, Cinnamon variant
+        LockCommand="$HASCSC"
+    elif [[ -n "$HASMSC" ]]; then
+        ## Linux Mint, Mate variant
+        LockCommand="$HASMSC"
+    fi
+    ## Did we find one?
+    if [[ -n "$LockCommand" ]]; then
+        echo "  Locking via $LockCommand"
+        "$LockCommand" --lock &
+    else
+        echo "  Locking failed - appropriate lock commnand not identified!"
+    fi
+else
+    echo "No locking requested. If you wish to also lock on suspend, create:
+  $doLockFile
+... with any content (at least one character)"
+fi
+
+if [[ -n "$ISSYSTEMD" ]]; then
     ## https://askubuntu.com/a/780990
     echo "Sleep by systemctl"
     systemctl suspend -i
     
-elif [[ ! -z "$HASPMI" ]]; then
+elif [[ -n "$HASPMI" ]]; then
     ## https://askubuntu.com/a/1795
     echo "Sleep by pmi"
     pmi action suspend
@@ -47,15 +77,4 @@ else
     
 fi
 
-# How can I suspend/hibernate from command line?
-# https://askubuntu.com/a/131022
-
-#if [ $HNAME == 'badger' ]; then
-#    # gnome-screensaver-command -l &
-#    dbus-send --type=method_call --dest=org.gnome.ScreenSaver \
-#        /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
-#fi
-
-echo "Waking up at " `date`
-
-sleep 5
+echo "Waking up at $(date)"

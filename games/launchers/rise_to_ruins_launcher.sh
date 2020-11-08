371844b3c841b6589b577418fa3b7fca734cf63e
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-11-08 18:33:01 -0500

    GOG version

--- games/launchers/rise_to_ruins_launcher.sh
+++ games/launchers/rise_to_ruins_launcher.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+## Location of the executable:
 PROGDIR="Rise to Ruins"
-LAUNCH="RtR.sh"
+LAUNCH="start.sh"
 
 ## Location of the installer on your file system:
-INSTDIR="IndieGameStand/$PROGDIR"
-INSTNAME='Rise to Ruins i25.zip'
+INSTDIR="GOG/$PROGDIR"
+INSTNAME='rise_to_ruins_english_*.sh'
+
+## Rename the default install directory to something else:
+## INSTRENAME='NameAfterUnpacking/'$PROGDIR
+INSTRENAME="$PROGDIR"' English/'"$PROGDIR"
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"
+
+## Filename of launcher icon (looks in default folders):
+INSTICON="$PROGDIR.png"
+## Location of the programs save files / config:
+INSTSAVEDIR="$HOME/Programs/$PROGDIR/game/profiles"
 
 find_and_run_executable "$@"

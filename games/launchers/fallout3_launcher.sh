165a4c6e08fc38c5f3ff5aaa7edbb7e1ed7c045a
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-12-06 11:23:35 -0500

    Failing - pushing to try installation on a different system

--- games/launchers/fallout3_launcher.sh
+++ games/launchers/fallout3_launcher.sh
@@ -12,15 +12,15 @@ INSTDIR="GOG/$PROGDIR"
 INSTNAME='setup_fallout_3_*.exe'
 
 ## Wine configuration:
-## wineBits="64"
-wineBits="32"
+wineBits="64"
+## wineBits="32"
 ## Getting messy enough that I want it in it's own prefix:
-WINEPREFIX="$HOME/WineFallout3"
+WINEPREFIX="$HOME/WineFallout3_$wineBits"
 WINEARCH="win$wineBits"
 APTPACKAGES="winetricks"
 ## https://appdb.winehq.org/objectManager.php?sClass=version&iId=14322
-INSTTRICKS="d3dx9 cc580 comctl32 directx9 dxdiagn l3codecx msasn1 msxml3 quartz vcrun2005 vcrun2008 vcrun2010 devenum vcrun2005sp1" 
-
+# INSTTRICKS="d3dx9 cc580 comctl32 directx9 dxdiagn l3codecx msasn1 msxml3 quartz vcrun2005 vcrun2008 vcrun2010 devenum vcrun2005sp1" 
+INSTTRICKS="d3dx9 cc580 vcrun2005 directx9 l3codecx quartz"
 ## Installation post-processing:
 WINETARGET="GOG Games/Fallout 3"
 ## INSTSAVEDIR="drive_c/GOG Games/Fallout/DATA/SAVEGAME"
@@ -30,6 +30,9 @@ INSTICON="$PROGDIR.png"
 PRERUN="
 WineHQ Platinum
   https://appdb.winehq.org/objectManager.php?sClass=version&iId=37374
+
+  Significant problems on low-spec computer - crashes during loading screen
+  on start of new game.
 "
 
 

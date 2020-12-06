83cf18682af1f8d368ff66a85bc17beb5da4ad29
Charles Tilford <gitcommit@biocode.fastmail.fm>
2020-12-06 11:23:04 -0500

    Failing to deal with some major input issues

--- games/launchers/noMansSky_launcher.sh
+++ games/launchers/noMansSky_launcher.sh
@@ -28,13 +28,17 @@ WINETARGET="GOG Games/$PROGDIR"
 INSTICON="$PROGDIR.png"
 
 ## Specify any required APT packages:
-APTPACKAGES='winbind mesa-vulkan-drivers libvulkan1'
+APTPACKAGES='winbind
+mesa-vulkan-drivers
+libvulkan1'
 
 ## Location of the programs save files / config:
 INSTSAVEDIR="drive_c/users/$USER/Application Data/HelloGames/NMS"
 
 ## Custom function to run after installation:
 function INSTFUNCTION {
+    ## Vulkan pacakged with game does not work. Rename it to cause
+    ## program to fall back to system's version of Vulkan:
     PDIR="$GAMEDIR/$PROGDIR/Binaries"
     GOOD="$PDIR/DO_NOT_USE_vulkan.dll"
     BAD="$PDIR/vulkan-1.dll"
@@ -55,5 +59,30 @@ Failed to find properly named Vulkan DLL. It should be at:
         exit
     fi
 }
+
+## Notes shown before program is executed
+PRERUN="
+WineHQ Gold, but Bronze in my hands:
+  https://appdb.winehq.org/objectManager.php?sClass=version&iId=34056
+
+Problem:
+  I can't for the life of me get mouse and keyboard to correctly work.
+  Most - BUT NOT ALL - controls can be remapped.
+  Mouse:
+    Right mouse button ends up just being left mouse button
+    Multiple options are mapped to Right Mouse. Most can be remapped
+      Construction Research Station REQUIRES RightMouse to exit interface
+        On first use you will be stuck here and have to force quit game,
+        losing progress. THIS IS GAME BREAKING. You can not progress story
+        line because of this.
+  Keyboard:
+    Modifier keys (like 'Left Ctrl') are not recognized.
+    In construction mode, this prevents deleting structures (Left Ctrl)
+
+Also, The Internet agrees that the GOG version has significant limitations
+when launched without GOG Galaxy - You will be known only as 'Explorer' and
+will not be able to participate in the community features. Multiplayer may
+also be messed up.
+"
 
 find_and_run_executable "$@"

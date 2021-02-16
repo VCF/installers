0e17ff574c151a1a8c37b31e577b6be9bb693dcf
Charles Tilford <gitcommit@biocode.fastmail.fm>
2021-02-16 10:27:10 -0500

    working

--- games/launchers/wolfensteinNewOrder_launcher.sh
+++ games/launchers/wolfensteinNewOrder_launcher.sh
@@ -2,11 +2,7 @@
 
 ## Location of the executable:
 PROGDIR="Wolfenstein - The New Order"
-LAUNCH="whatActuallyRunsTheProgram.exe"
-## Arguments to pass to the executable when running:
-## LAUNCHARGS="-nogui -res=640x320"
-## Subdirectory in the program folder to change to before running:
-## PROGSUBDIR="bin"
+LAUNCH="WolfNewOrder_x64.exe"
 
 ## Location of the installer on your file system:
 INSTDIR="GOG/$PROGDIR"
@@ -24,40 +20,21 @@ WINEPREFIX="$HOME/Wine$wineBits"
 ## Set the architecture being used for Wine:
 WINEARCH="win$wineBits"
 ## The location of the installed program in drive_c:
-WINETARGET="Program Files/Foo Bar"
-## Any winetricks that might be needed:
-## INSTTRICKS="d3dx9_43 wmp9 wmp10"
-## Switches passed to Wine when running application
-## WINEARGS="-windowed"
+WINETARGET="GOG Games/Wolfenstein The New Order"
 
 ## Filename of launcher icon (looks in default folders):
 INSTICON="$PROGDIR.png"
 
-## Specify any required APT packages:
-## APTPACKAGES='libsdl2-mixer-2.0-0'
-## Normally a new terminal is opened to launch. Flag to prevent this:
-## NOTINTERM="No launching terminal"
-
 ## Location of the programs save files / config:
-## INSTSAVEDIR="$HOME/.config/foobar"
+INSTSAVEDIR="drive_c/users/$USER/Saved Games/MachineGames/Wolfenstein The New Order"
 ## Flag to use if backup should use rsync rather than tar/gzip:
 ## DORSYNC="yes"
-## Flag that prevents backups if set, and shows the provided message:
-## NOAUTOBACK="Backups take too long to run"
-
-## Custom function to run after installation:
-#function INSTFUNCTION {
-#  echo "For example, properly setting executable bits"
-#}
-## Custom function to run each time just before running the program:
-#function RUNFUNCTION {
-#  echo "For example, configuring the system in some way"
-#}
 
 ## Notes shown before program is executed
 PRERUN="
 Wine Platinum
   https://appdb.winehq.org/objectManager.php?sClass=version&iId=39251
+  Flawless on mid-end machine
 "
 
 find_and_run_executable "$@"

da196d3db598f7276f23549ac4a13a52732a75e0
Charles Tilford <gitcommit@biocode.fastmail.fm>
2021-03-05 20:04:55 -0500

    Far - Lone Sails

--- games/launchers/farLoneSails_launcher.sh
+++ games/launchers/farLoneSails_launcher.sh
@@ -3,14 +3,10 @@
 ## Location of the executable:
 PROGDIR="Far - Lone Sails"
 LAUNCH="FarLoneSails.exe"
-## Arguments to pass to the executable when running:
-## LAUNCHARGS="-nogui -res=640x320"
-## Subdirectory in the program folder to change to before running:
-## PROGSUBDIR="bin"
 
 ## Location of the installer on your file system:
 INSTDIR="GOG/$PROGDIR"
-INSTNAME='setup_far_lone_sails_*.exe'
+INSTNAME='setup_far_lone_sails_1.21_(33080).exe'
 
 ## Source Utility functions
 my_dir="$(dirname "$0")"
@@ -25,42 +21,19 @@ WINEPREFIX="$HOME/Wine$wineBits"
 WINEARCH="win$wineBits"
 ## The location of the installed program in drive_c:
 WINETARGET="GOG Games/FAR Lone Sails"
-## Any winetricks that might be needed:
-## INSTTRICKS="d3dx9_43 wmp9 wmp10"
-## Switches passed to Wine when running application
-## WINEARGS="-windowed"
 
 ## Filename of launcher icon (looks in default folders):
 INSTICON="$PROGDIR.png"
-
-## Specify any required APT packages:
-## APTPACKAGES='libsdl2-mixer-2.0-0'
-## Normally a new terminal is opened to launch. Flag to prevent this:
-## NOTINTERM="No launching terminal"
 
 ## Location of the programs save files / config:
 ## INSTSAVEDIR="$HOME/.config/foobar"
 ## Flag to use if backup should use rsync rather than tar/gzip:
 ## DORSYNC="yes"
-## Flag that prevents backups if set, and shows the provided message:
-## NOAUTOBACK="Backups take too long to run"
-
-## Custom function to run after installation:
-#function INSTFUNCTION {
-#  echo "For example, properly setting executable bits"
-#}
-## Custom function to run each time just before running the program:
-#function RUNFUNCTION {
-#  echo "For example, configuring the system in some way"
-#}
 
 ## Notes shown before program is executed
 PRERUN="
-Wine Platinum
-  https://appdb.winehq.org/objectManager.php?sClass=version&iId=36986
-
-  Graphics glitch on integrated graphics system:
-  Perpetual black loading screen (leave with ESC)
+Wine 64-bit
+Landing page was blank on low-spec integrated graphics system
 "
 
 find_and_run_executable "$@"

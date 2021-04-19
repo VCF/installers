85bd24119281e68ef59762505576fdc2b17984d4
Charles Tilford <gitcommit@biocode.fastmail.fm>
2021-04-19 13:00:02 -0400

    Total Annihilation

---
+++ games/launchers/totalAnnihilation_launcher.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+## Location of the executable:
+PROGDIR="Total Annihilation"
+LAUNCH="TotalA.exe"
+
+## Location of the installer on your file system:
+INSTDIR="GOG/$PROGDIR"
+INSTNAME='setup_total_annihilation_commander_pack_*.exe'
+
+## Source Utility functions
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+###### configuration:
+## Should we use 32- or 64-bit environment?
+wineBits="32"
+## Define the prefix (folder) for the Wine installation:
+WINEPREFIX="$HOME/Wine$wineBits"
+## Set the architecture being used for Wine:
+WINEARCH="win$wineBits"
+## The location of the installed program in drive_c:
+WINETARGET="GOG Games/Total Annihilation - Commander Pack"
+
+## Filename of launcher icon (looks in default folders):
+INSTICON="$PROGDIR.png"
+
+## Location of the programs save files / config:
+INSTSAVEDIR="$GAMEDIR/$PROGDIR/SAVEGAME"
+
+## Notes shown before program is executed
+PRERUN="
+Wine Platinum:
+  https://appdb.winehq.org/objectManager.php?sClass=application&iId=155
+"
+
+find_and_run_executable "$@"

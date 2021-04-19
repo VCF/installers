db06eeb86c16c083d12160362ca55297c8b30c41
Charles Tilford <gitcommit@biocode.fastmail.fm>
2021-04-19 13:24:44 -0400

    New libsdl2 library requirements

--- games/launchers/cataclysmdda_launcher.sh
+++ games/launchers/cataclysmdda_launcher.sh
@@ -14,6 +14,9 @@ libsdl2-dev
 libsdl2-ttf-dev
 libsdl2-image-dev
 libsdl2-mixer-dev
+libsdl2-mixer-2.0-0
+libsdl2-ttf-2.0-0
+libsdl2-image-2.0-0
 libfreetype6-dev
 build-essential"
 INSTSAVEDIR="$HOME/Programs/$PROGDIR/save"

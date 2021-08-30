fb6ed62bf5990773df4117df0b0c1970c8a41412
Charles Tilford <gitcommit@biocode.fastmail.fm>
2021-08-29 22:19:07 -0400

    folder rename

--- games/launchers/skyrogue_launcher.sh
+++ games/launchers/skyrogue_launcher.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+## Location of the executable:
 PROGDIR="skyrogue_linux"
 LAUNCH="skyrogue.x86_64"
 
 ## Location of the installer on your file system:
-INSTDIR="IndieGameStand/$PROGDIR"
+INSTDIR="IndieGameStand/Sky Rogue"
 INSTNAME='skyrogue_*_linux.zip'
+
+INSTICON="SkyRogue.jpg"
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"

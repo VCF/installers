7cb79e315751ad5f14ca933669b9609fda193e62
Charles Tilford <gitcommit@biocode.fastmail.fm>
2021-12-11 00:20:50 -0500

    installer rename, save location

--- games/launchers/xenonauts_launcher.sh
+++ games/launchers/xenonauts_launcher.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+## Location of the executable:
 PROGDIR="Xenonauts"
-LAUNCH="Xenonauts.bin.x86"
+LAUNCH="start.sh"
 
 ## Location of the installer:
 INSTDIR="GOG/$PROGDIR"
-INSTNAME='setup_xenonauts_*.bin'
+INSTNAME='xenonauts_en_*.sh'
 
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"
+
+## Filename of launcher icon (looks in default folders):
+INSTICON="$PROGDIR.png"
+
+## Location of the programs save files / config:
+INSTSAVEDIR="$HOME/.local/share/Goldhawk Interactive/Xenonauts"
+## Flag to use if backup should use rsync rather than tar/gzip:
+DORSYNC="yes"
 
 find_and_run_executable "$@"

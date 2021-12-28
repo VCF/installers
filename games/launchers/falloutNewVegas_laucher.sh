29abad66b5501453f16faf4d682f81d3b08921d2
Charles Tilford <gitcommit@biocode.fastmail.fm>
2021-12-28 12:53:53 -0500

    Fallout New Vegas

---
+++ games/launchers/falloutNewVegas_laucher.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+## Location of the executable:
+PROGDIR="Fallout New Vegas"
+LAUNCH="FalloutNVLauncher.exe"
+
+## Location of the installer on your file system:
+INSTDIR="GOG/$PROGDIR"
+INSTNAME='setup_fallout_new_vegas_*.exe'
+
+## Source Utility functions
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+###### configuration:
+## Should we use 32- or 64-bit environment?
+wineBits="32"
+## Define the prefix (folder) for the Wine installation:
+WINEPREFIX="$HOME/Wine$wineBits"
+## Set the architecture being used for Wine:
+WINEARCH="win$wineBits"
+## The location of the installed program in drive_c:
+WINETARGET="GOG Games/Fallout New Vegas"
+
+## Filename of launcher icon (looks in default folders):
+INSTICON="$PROGDIR.png"
+
+## Location of the programs save files / config:
+INSTSAVEDIR="$HOME/Documents/My Games/FalloutNV"
+## Flag to use if backup should use rsync rather than tar/gzip:
+DORSYNC="yes"
+
+## Notes shown before program is executed
+PRERUN="
+Wine Gold
+  https://appdb.winehq.org/objectManager.php?sClass=version&iId=35340
+"
+
+find_and_run_executable "$@"

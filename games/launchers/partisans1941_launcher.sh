1f6dad0726fe81195bb90370c4d0c84781756284
Charles Tilford <gitcommit@biocode.fastmail.fm>
2022-04-17 14:30:57 -0400

    Partisans 1941

--- games/launchers/partisans1941_launcher.sh
+++ games/launchers/partisans1941_launcher.sh
@@ -25,45 +25,22 @@ WINEPREFIX="$HOME/Wine$wineBits"
 WINEARCH="win$wineBits"
 ## The location of the installed program in drive_c:
 WINETARGET="GOG Games/Partisans 1941"
-## Any winetricks that might be needed:
-## INSTTRICKS="d3dx9_43 wmp9 wmp10"
-## Switches passed to Wine when running application
-## WINEARGS="-windowed"
 
 ## Filename of launcher icon (looks in default folders):
 INSTICON="$PROGDIR.png"
 
-## Specify any required APT packages:
-## APTPACKAGES='libsdl2-mixer-2.0-0'
-## Normally a new terminal is opened to launch. Flag to prevent this:
-## NOTINTERM="No launching terminal"
-
 ## Location of the programs save files / config:
-## INSTSAVEDIR="$HOME/.config/foobar"
+INSTSAVEDIR="drive_c/users/$USER/Local Settings/Application Data/Partisans/Saved/SaveGames"
 ## Flag to use if backup should use rsync rather than tar/gzip:
-## DORSYNC="yes"
-## Flag that prevents backups if set, and shows the provided message:
-## NOAUTOBACK="Backups take too long to run"
-
-## Custom function to run each time just BEFORE running the program:
-#function RUNFUNCTION {
-#}
-## Custom function to run AFTER program executes
-#function ENDFUNCTION {
-#    ## eg clean up system after program has tinkered with it
-#}
-## Custom function to run after installation:
-#function INSTFUNCTION {
-#  echo "For example, properly setting executable bits"
-#  addDLC 'foobar_pro_DLC_pack_*.exe'
-#}
+DORSYNC="yes"
 
 ## Notes shown before program is executed
 PRERUN="
 Wine Platinum
   https://www.protondb.com/app/1227530
 
-Currently not working: fails to find .uproject file
+Worked on higher-end system
+Failed on fanless system: fails to find .uproject file
 "
 
 find_and_run_executable "$@"

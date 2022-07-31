0dd6a9d6d6d34a7b2274837d577ade4eebc1924c
Charles Tilford <gitcommit@biocode.fastmail.fm>
2022-07-31 18:23:58 -0400

    Merc Tactics - glibc too low on Mint 19.3

---
+++ games/launchers/mercTactics_launcher.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+## Location of the executable:
+PROGDIR="Merc Tactics"
+LAUNCH="MercTactics"
+## Arguments to pass to the executable when running:
+## LAUNCHARGS="-nogui -res=640x320"
+## Subdirectory in the program folder to change to before running:
+## PROGSUBDIR="bin"
+
+## Location of the installer on your file system:
+INSTDIR="GOG/$PROGDIR"
+INSTNAME='MercTactics*.tar.gz'
+
+## Source Utility functions
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+## Filename of launcher icon (looks in default folders):
+INSTICON="$PROGDIR.png"
+
+###### Unpacking optionos
+## Parameters for archives (zip/gz):
+## Define a parent directory for unpacking archives:
+## UNPACKDIR="$PROGDIR"
+## Rename the default install directory to something else:
+INSTRENAME='MercTacticsDemo/'"$PROGDIR"
+
+## Specify any required APT packages:
+## APTPACKAGES='libsdl2-mixer-2.0-0'
+## Normally a new terminal is opened to launch. Flag to prevent this:
+## NOTINTERM="No launching terminal"
+
+## Location of the programs save files / config:
+## INSTSAVEDIR="$HOME/.config/foobar"
+## Flag to use if backup should use rsync rather than tar/gzip:
+## DORSYNC="yes"
+## Flag that prevents backups if set, and shows the provided message:
+## NOAUTOBACK="Backups take too long to run"
+
+## Custom function to run after installation:
+#function INSTFUNCTION {
+#  echo "For example, properly setting executable bits"
+#}
+## Custom function to run each time just before running the program:
+#function RUNFUNCTION {
+#  echo "For example, configuring the system in some way"
+#}
+
+## Notes shown before program is executed
+PRERUN="
+Requires up-to-date version of glibc
+"
+
+find_and_run_executable "$@"

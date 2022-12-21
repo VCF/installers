1a341bfe2f45a81e784b2e3289dcd81b8e26cae7
Charles Tilford <gitcommit@biocode.fastmail.fm>
2022-12-21 14:18:37 -0500

    basic version seems to work

--- games/launchers/7daysSafetyBackup.sh
+++ games/launchers/7daysSafetyBackup.sh
@@ -12,8 +12,30 @@ the player back to Level 1, and always reverts the world back to Day 1.
 This script is designed to make frequent backups of the core file to
 preserve btoth player level as well as game day.
 
+---------------------------------------------------------------------------
+
+To have this script run automatically, add it to your cron jobs by running:
+
+crontab -e
+
+... and then adding the following line:
+
+*/5 * * * * $HOME/installers/games/launchers/7daysSafetyBackup.sh
+
+That will run every 5 minutes and make a copy of the main.ttw
+file. You will have at most 12 such files covering an hour of game
+time. You may need to change the file path if this file is in a
+different location.
 
 "
+
+# The first argument is where the backup files should be saved
+#   It defaults to '7daystodie_backups' in your home folder
+targDir="${1:-$HOME/7daystodie_backups}"
+
+# The second argument is where the game normally saves files
+#   It defaults to the standard location (as of Dec 2022)
+srcDir="${2:-$HOME/.local/share/7DaysToDie/Saves}"
 
 ## Be more chatty if this is being run by a human (ie not cron)
 vb=""
@@ -25,23 +47,30 @@ fi
 # We only execute when 7 Days is running
 #   Check if executable is present - https://stackoverflow.com/a/9271229
 gamePid=$(pgrep '7DaysToDie')
+## File noting that game is running:
+wasRun="$targDir/isRunning.txt"
 
 if [[ -z "$gamePid" ]]; then
     # Nope, looks like no active processes.
     if [[ $vb == "" ]]; then
-        exit # Just quit for non-interactive sessions
+        ## We're not in interactive mode, presumably cron-launched
+        if [[ -f "$wasRun" ]]; then
+            ## Not running now, but was just one cycle ago. We'll
+            ## carry on one more time to see if anything needs
+            ## updating
+            rm "$wasRun" # Remove run file to cause exit on next iteration
+        else
+            exit # Just quit for non-interactive sessions
+        fi
     else
-        echo "[INFO] 7 Days is not currently running"
+        echo "
+[INFO] 7 Days is not currently running"
     fi
+else
+    # Note that the game is running by creating a small file. This is
+    # to allow one last synchronization after the game finishes
+    echo "Last seen running at $(date)" > "$wasRun"
 fi
-
-# The first argument is where the backup files should be saved
-#   It defaults to '7daystodie_backups' in your home folder
-targDir="${1:-$HOME/7daystodie_backups}"
-
-# The second argument is where the game normally saves files
-#   It defaults to the standard location (as of Dec 2022)
-srcDir="${2:-$HOME/.local/share/7DaysToDie/Saves}"
 
 if [[ ! -d "$srcDir" ]]; then
     ## The source directory does not exist?
@@ -64,19 +93,21 @@ basettw="main.ttw"
 ## preserve that structure in the backups
 [[ $vb != "" ]] && echo "
 Backing up: $srcDir
+        to: $targDir
 "
-
+dirsDone=""
 ## Split string on newlines: https://stackoverflow.com/a/19772067
 IFS=$'\n' read -rd '' -a worldDirs <<< "$(ls -1 "$srcDir")"
 for wd in "${worldDirs[@]}"
 do
     sw="$srcDir/$wd" # World directory in saves
-    mkdir -p "$sw" 
+    [[ ! -d "$sw" ]] && continue # Wasn't a directory
+    [[ $vb != "" ]] && echo "World \"$wd\""
     ## Now find the game directories in this world
     IFS=$'\n' read -rd '' -a gameDirs <<< "$(ls -1 "$sw")"
     for gd in "${gameDirs[@]}"
     do
-        [[ $vb != "" ]] && echo "World \"$wd\" Game \"$gd\""
+        [[ $vb != "" ]] && echo "  Game \"$gd\""
         swg="$sw/$gd"           # Game directory in saves
         ttw="$swg/$basettw"     # The key file we're looking for
         if [[ ! -f "$ttw" ]]; then
@@ -89,7 +120,7 @@ do
         mkdir -p "$twg"         # Assure it exists
 
         ## What is the most recent backup file already there?
-        latest="$(ls -1t "$twg/*.ttw")"
+        latest="$(ls -1t "$twg/"*.ttw 2>/dev/null | head -n 1 )"
 
         fn="$twg/${minFlag}-$basettw" # The backup file we're planning to make
         fmd5="${fn}.md5"              # MD5 checksum file of the above file
@@ -110,7 +141,7 @@ do
         if [[ -f "$latest" ]]; then
             ## OK, we found the most recent file. Is it different than
             ## the file we just copied?
-            if [[ cmp --silent "${latest}.md5" "$f5tmp" ]]; then
+            if cmp --silent "${latest}.md5" "$f5tmp"; then
                 ## The checksums appear to be identical. We do not need
                 ## to do anything, other than clear the temp files
                 rm "$fntmp" "$f5tmp"
@@ -121,6 +152,16 @@ do
         ## This file has changed. Keep it after removing the .tmp suffix
         mv "$fntmp" "$fn"
         mv "$f5tmp" "$fmd5"
+        dirsDone="$dirsDone
+$ttw
+"
         [[ $vb != "" ]] && echo "    [BACKUP] \"$(ls -sh "$fn")\""
     done
 done
+
+echo "
+Last Run: $(date)
+
+Changes Found:
+$dirsDone
+" > "$targDir/Readme.txt"

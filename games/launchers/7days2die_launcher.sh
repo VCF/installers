8803ecadf88b7aca306675171d57970e990d5aa4
Charles Tilford <gitcommit@biocode.fastmail.fm>
2023-03-04 17:28:52 -0500

    Also launching steam, if needed

--- games/launchers/7days2die_launcher.sh
+++ games/launchers/7days2die_launcher.sh
@@ -3,6 +3,25 @@
 PROGDIR="7 Days to Die"
 STEAMID="251570"
 
+PRERUN="
+7 Days to Die can irreversibly corrupt your save data if the application
+crashes (about once every 30-50 hours in the author's experience) or if you
+lose power. Your save games will be backed up BEFORE each launch to allow
+recovery from such events, at least to the state before you started the
+game. For every World/Game combination, at most $keepFiles saves will be kept,
+stored as gzipped tarballs.
+"
+
+POSTRUN="
+Your game was backed up just before you started. When you run the launcher
+again, it will make a new backup of the current game state. If you would
+like to make another backup now, run the launcher with an additional
+argument 'help'.
+
+The game is NOT backed up by default AFTER running because there's a non-trivial
+chance that the save has been corrupted. :-(
+"
+
 ## Source Utility functions
 my_dir="$(dirname "$0")"
 . "$my_dir/_launcher_functions.sh"
@@ -24,21 +43,29 @@ if [[ ! -d "$srcDir" ]]; then
     exit
 fi
 
+## pgrep was new to me ...
+##   https://stackoverflow.com/a/6225730
+STEAMUP="$(pgrep steam)"
 
+if [[ "$STEAMUP" == "" ]]; then
+    ## If steam was not already running, it will launch and be tied to this
+    ## shell, which can be annoying. So start steam in the background
+    msg "$FgCyan" "Starting Steam in the background ..."
+
+    ## Truly push to background with disown and (or?) nohup
+    ##   https://stackoverflow.com/a/29681504
+    ##   https://unix.stackexchange.com/a/269899
+    nohup steam &>/dev/null & disown;
+fi
+
+## Defines the resolution of the tar.gz filename:
 backupGranularity "second"
-
-PRERUN="
-7 Days to Die can irreversibly corrupt your save data if the application
-crashes (about once every 100 hours in the author's experience) or if you
-lose power. Your save games will be backed up before each launch to allow
-recovery from such events, at least to the state before you started the
-game. For every World/Game combination, at most 3 saves will be kept, stored
-as gzipped tarballs.
-"
+## How many backups do we keep at any given time?
+keepFiles=5
 
 ## Split string on newlines: https://stackoverflow.com/a/19772067
 msg "$FgCyan" "Backing up worlds before game run ..."
-lnkTxt="Link to this worlds save game folder"
+lnkTxt="Link to actual Steam folder"
 IFS=$'\n' read -rd '' -a worldDirs <<< "$(ls -1 "$srcDir")"
 for wd in "${worldDirs[@]}"
 do
@@ -52,27 +79,16 @@ do
         swg="$sw/$gd"           # Game directory in saves
         subfolder="7days/$gd__$wd"
         archiveFolder "$swg" "$subfolder" # Archive this world-game, if needed
-        trimBackups "$swg" "$subfolder" 3 # Keep only 3 most recent backups
+        trimBackups "$swg" "$subfolder" $keepFiles # Keep at most keepFiles
         ## Make a symlink back to save folder
         sfpath=$(backupSubfolder "$swg" "$subfolder")
         lpath="$sfpath/$lnkTxt"
         if [[ ! -L "$lpath" ]]; then
-            ln -s "$swg" "$lpath"
+            ln -s "$sw" "$lpath"
         fi
     done
 done
 echo ""
-
-POSTRUN="
-Your game was backed up just before you started. When you run the launcher
-again, it will make a new backup of the current game state. If you would
-like to make another backup now, run the launcher with an additional
-argument 'help'.
-
-The game is NOT backed up by default after running because there's a non-trivial
-chance that the save has been corrupted. :-(
-"
-
 
 desktopIcon
 find_and_run_executable "$@"

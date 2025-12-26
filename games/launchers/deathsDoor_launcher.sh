956d8cd77075c1a43cec674e04fd6790bf3fbb65
Charles Tilford <gitcommit@biocode.fastmail.fm>
2025-12-25 19:50:55 -0500

    Death's Door wine wrapper

---
+++ games/launchers/deathsDoor_launcher.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+## Location of the executable:
+PROGDIR="Death's Door"
+LAUNCH="DeathsDoor.exe"
+
+## Location of the installer on your file system:
+INSTDIR="GOG/Deaths Door"
+INSTNAME='setup_deaths_door_*.exe'
+
+## Source Utility functions
+my_dir="$(dirname "$0")"
+. "$my_dir/_launcher_functions.sh"
+
+###### configuration:
+## Should we use 32- or 64-bit environment?
+wineBits="64"
+## Define the prefix (folder) for the Wine installation:
+WINEPREFIX="$HOME/Wine$wineBits"
+## Set the architecture being used for Wine:
+WINEARCH="win$wineBits"
+## The location of the installed program in drive_c:
+WINETARGET="GOG Games/$PROGDIR"
+
+## Filename of launcher icon (looks in default folders):
+INSTICON="$PROGDIR.png"
+
+## Location of the programs save files / config:
+INSTSAVEDIR="drive_c/users/tilfordc/AppData/LocalLow/Acid Nerve/DeathsDoor/SAVEDATA"
+## Flag to use if backup should use rsync rather than tar/gzip:
+## DORSYNC="yes"
+## Flag that prevents backups if set, and shows the provided message:
+## NOAUTOBACK="Backups take too long to run"
+
+## Notes shown before program is executed
+PRERUN="
+Wine Gold
+  https://www.protondb.com/app/894020?device=pc
+"
+
+find_and_run_executable "$@"
